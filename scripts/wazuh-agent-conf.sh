7bb7ab67a656e7380978b6e6f5a30cd78489ea46
root <root@r3.siua.ac.cr>
2026-07-30 03:46:00 +0000

    Agente remoto probado, OJO resolver que el dashboard debe esperar a que el indexer este 100% listo porque si no se correompen los indices y el servidor web dice que aun no esta listo y asi se queda

--- scripts/wazuh-agent-conf.sh
+++ scripts/wazuh-agent-conf.sh
@@ -14,14 +14,23 @@ if [ -f /var/ossec/etc/authd.pass ]; then
 fi
 
 # Install docker-listener dependencies only if missing (avoids reinstalling on each restart)
-if ! python3 -c "import docker" >/dev/null 2>&1; then
-    echo "📦 Installing Python dependencies for docker-listener..."
-    dnf install -y python3-pip
-    pip3 install docker==7.1.0 urllib3==1.26.20 requests==2.32.2
-    echo "✅ Docker-listener dependencies installed."
-else
-    echo "✅ Docker-listener dependencies already present, skipping installation."
-fi
+#if ! python3 -c "import docker" >/dev/null 2>&1; then
+#    echo "📦 Installing Python dependencies for docker-listener..."
+#    dnf install -y python3-pip
+#    pip3 install docker==7.1.0 urllib3==1.26.20 requests==2.32.2
+#    echo "✅ Docker-listener dependencies installed."
+#else
+#    echo "✅ Docker-listener dependencies already present, skipping installation."
+#fi
 
+if [ -n "$1" ]; then
+    echo "Wazuh manager server: $1"
+    cp /var/ossec/etc/ossec.conf /tmp/ossec.conf
+    sed -i "/<server>/,/<\/server>/ s|<address>.*</address>|<address>${1}</address>|" \
+    /tmp/ossec.conf
+    cat /tmp/ossec.conf > /var/ossec/etc/ossec.conf
+    rm /tmp/ossec.conf
+    shift
+fi
 # Execute the original entrypoint of the agent image
 exec /init "$@"
